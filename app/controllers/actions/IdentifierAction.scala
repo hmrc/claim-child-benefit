e9b93914b5697399eb6c67315fa94620c4f11eca
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-11-07 08:37:55 +0000

    Add action to identify user by session id

---
+++ app/controllers/actions/IdentifierAction.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import models.IdentifierRequest
+import play.api.mvc.Results.BadRequest
+import play.api.mvc.{ActionBuilder, ActionFunction, AnyContent, BodyParsers, Request, Result}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class IdentifierAction @Inject()(val parser: BodyParsers.Default)
+                                (implicit val executionContext: ExecutionContext)
+  extends ActionBuilder[IdentifierRequest, AnyContent] with ActionFunction[Request, IdentifierRequest]{
+
+  override def invokeBlock[A](request: Request[A], block: IdentifierRequest[A] => Future[Result]): Future[Result] = {
+
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+
+    hc.sessionId
+      .map(sessionId => block(IdentifierRequest(request, sessionId.value)))
+      .getOrElse(Future.successful(BadRequest))
+  }
+}
